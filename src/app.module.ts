76011eed1ec7c28a6b94d51c92c53de95c4f7c36
zpskek <wjdghdwns0@gmail.com>
2021-03-21 13:19:06 +0900

    Configuring ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
-    RestaurantsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'hongjun',
-      password: '12345', // Postgresql은 localhost에 연결된 경우, password를 안 물어본다.
-      database: 'bixby-eats',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD, // Postgresql은 localhost에 연결된 경우, password를 안 물어본다.
+      database: process.env.DB_NAME,
       synchronize: true, // TypeORM이 DB에 연결할 때 DB를 나의 모듈의 현재 상태로 migration한다는 뜻이다.
       logging: true, // DB에 무슨 일이 일어나는지 콘솔에 표시하는 거다.
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
