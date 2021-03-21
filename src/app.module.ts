12db00d56ffc4b8654a5656fad59472d963ed76f
zpskek <wjdghdwns0@gmail.com>
2021-03-21 15:39:42 +0900

    Injecting the Repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { Restaurant } from './restaurants/entities/restaurant.entity';
 
 @Module({
   imports: [
@@ -29,8 +30,9 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD, // Postgresql은 localhost에 연결된 경우, password를 안 물어본다.
       database: process.env.DB_NAME,
-      synchronize: true, // TypeORM이 DB에 연결할 때 DB를 나의 모듈의 현재 상태로 migration한다는 뜻이다.
+      synchronize: process.env.NODE_ENV !== 'prod', // TypeORM이 DB에 연결할 때 DB를 나의 모듈의 현재 상태로 migration한다는 뜻이다.
       logging: true, // DB에 무슨 일이 일어나는지 콘솔에 표시하는 거다.
+      entities: [Restaurant],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,30 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@ObjectType()
+@ObjectType() // GraphQL을 위한 code
+@Entity() // typeORM을 위한 code
 export class Restaurant {
-  @Field((type) => String)
+  @PrimaryGeneratedColumn()
+  @Field((type) => Number)
+  id: number;
+
+  @Field((type) => String) // GraphQL은 Field
+  @Column() // TypeORM은 Column을 사용한다.
   name: string;
 
   @Field((type) => Boolean)
+  @Column()
   isVegan: boolean;
 
   @Field((type) => String)
+  @Column()
   address: string;
 
   @Field((type) => String)
+  @Column()
   ownersName: string;
+
+  @Field((type) => String)
+  @Column()
+  categoryName: string;
 }
