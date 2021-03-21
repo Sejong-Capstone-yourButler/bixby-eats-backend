593e8c4018cf1d76449470ae844f495314e1e3b6
zpskek <wjdghdwns0@gmail.com>
2021-03-22 05:57:45 +0900

    Create update-restaurant

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantService } from './restaurants.service';
 
@@ -18,6 +19,21 @@ export class RestaurantResolver {
   ): Promise<boolean> {
     try {
       await this.restaurantService.createRestaurant(createRestaurantDto);
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+
+  // dto.ts에서 @ArgsType를 쓰면 @Args()로 empty해야 하고
+  // dto.ts에서 @InputType을 쓰면 @args('input')이어야 한다.
+  @Mutation((returns) => Boolean)
+  async updateRestaurant(
+    @Args('input') updateRestaurantDto: UpdateRestaurantDto,
+  ): Promise<boolean> {
+    try {
+      await this.restaurantService.updateRestaurant(updateRestaurantDto);
       return true;
     } catch (e) {
       console.log(e);
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Injectable()
@@ -19,5 +20,9 @@ export class RestaurantService {
     // 단순히 create만 하면 TS에만 존재하고 DB에는 저장되지 않으니 .save()를 사용해야 한다.
     const newRestaurant = this.restaurants.create(createRestaurantDto);
     return this.restaurants.save(newRestaurant);
+  }
+  updateRestaurant({ id, data }: UpdateRestaurantDto) {
+    // .update()에서 첫번째 arg는 search criteria다.
+    return this.restaurants.update(id, { ...data });
   }
 }
