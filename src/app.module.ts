f167564e3e2dbe376cda18d25e1f86ba5e80c336
zpskek <wjdghdwns0@gmail.com>
2021-03-27 15:49:37 +0900

    Create Restaurant models

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,8 @@ import { JwtModule } from './jwt/jwt.module';
 import { JwtMiddleware } from './jwt/jwt.middleware';
 import { Verification } from './users/entities/verification.entity';
 import { MailModule } from './mail/mail.module';
+import { Restaurant } from './restaurants/entities/restaurant.entity';
+import { Category } from './restaurants/entities/category.entity';
 
 @Module({
   imports: [
@@ -52,7 +54,7 @@ import { MailModule } from './mail/mail.module';
       // DB에 무슨 일이 일어나는지 콘솔에 표시하는 거다.
       logging:
         process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
-      entities: [User, Verification],
+      entities: [User, Verification, Restaurant, Category],
     }),
     // GraphQLModule처럼 설정이 있으면 Dynamic Module이다.
     GraphQLModule.forRoot({
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,29 +1,30 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { Category } from './category.entity';
 
 @InputType({ isAbstract: true }) // Dto를 위한 code
 @ObjectType() // GraphQL을 위한 code
 @Entity() // typeORM을 위한 code
-export class Restaurant {
-  @PrimaryGeneratedColumn()
-  @Field((type) => Number)
-  id: number;
-
+export class Restaurant extends CoreEntity {
   @Field((type) => String) // GraphQL은 Field
   @Column() // TypeORM은 Column을 사용한다.
   @IsString()
   @Length(5)
   name: string;
 
-  @Field((type) => Boolean, { nullable: true })
-  @Column({ default: false })
-  @IsOptional()
-  @IsBoolean()
-  isVegan: boolean;
+  @Field((type) => String)
+  @Column()
+  @IsString()
+  coverImg: string;
 
   @Field((type) => String, { defaultValue: '강남' })
   @Column()
   @IsString()
   address: string;
+
+  @Field((type) => Category)
+  @ManyToOne((type) => Category, (category) => category.restaurants)
+  category: Category;
 }
