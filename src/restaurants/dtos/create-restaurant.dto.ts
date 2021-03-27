c42fad14b599e335874b580a0c2655b2e98183ae
zpskek <wjdghdwns0@gmail.com>
2021-03-27 22:41:04 +0900

    Create createRestaurant

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,4 +1,5 @@
-import { InputType, OmitType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
+import { CoreOutput } from 'src/common/dtos/output.dto';
 import { Restaurant } from '../entities/restaurant.entity';
 
 // @ArgsType()
@@ -24,4 +25,14 @@ import { Restaurant } from '../entities/restaurant.entity';
 // Mapped Type으로 dto를 entity와 연동할 수 있다.
 // entity.ts 파일의 정의대로 dto가 정의된다.
 @InputType()
-export class CreateRestaurantDto extends OmitType(Restaurant, ['id']) {}
+export class CreateRestaurantInput extends PickType(Restaurant, [
+  'name',
+  'coverImg',
+  'address',
+]) {
+  @Field((type) => String)
+  categoryName: string;
+}
+
+@ObjectType()
+export class CreateRestaurantOutput extends CoreOutput {}
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
-import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
-import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
+import {
+  CreateRestaurantInput,
+  CreateRestaurantOutput,
+} from './dtos/create-restaurant.dto';
+import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Injectable()
@@ -10,19 +14,42 @@ export class RestaurantService {
   constructor(
     @InjectRepository(Restaurant)
     private readonly restaurants: Repository<Restaurant>,
+
+    @InjectRepository(Category)
+    private readonly categories: Repository<Category>,
   ) {}
-  getAll(): Promise<Restaurant[]> {
-    return this.restaurants.find();
+
+  async createRestaurant(
+    owner: User,
+    createRestaurantInput: CreateRestaurantInput,
+  ): Promise<CreateRestaurantOutput> {
+    try {
+      // 단순히 create만 하면 TS에만 존재하고 DB에는 저장되지 않으니 .save()를 사용해야 한다.
+      const newRestaurant = this.restaurants.create(createRestaurantInput);
+      newRestaurant.owner = owner;
+      const categoryName = createRestaurantInput.categoryName
+        .trim()
+        .toLowerCase();
+      const categorySlug = categoryName.replace(/ /g, '-');
+      let category = await this.categories.findOne({ slug: categorySlug });
+      if (!category) {
+        category = await this.categories.save(
+          this.categories.create({ slug: categorySlug, name: categoryName }),
+        );
+      }
+      newRestaurant.category = category;
+
+      await this.restaurants.save(newRestaurant);
+      return {
+        ok: true,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not create restaurant',
+      };
+    }
   }
-  createRestaurant(
-    createRestaurantDto: CreateRestaurantDto,
-  ): Promise<Restaurant> {
-    // 단순히 create만 하면 TS에만 존재하고 DB에는 저장되지 않으니 .save()를 사용해야 한다.
-    const newRestaurant = this.restaurants.create(createRestaurantDto);
-    return this.restaurants.save(newRestaurant);
-  }
-  updateRestaurant({ id, data }: UpdateRestaurantDto) {
-    // .update()에서 첫번째 arg는 search criteria다.
-    return this.restaurants.update(id, { ...data });
-  }
+
+  // .update()에서 첫번째 arg는 search criteria다.
 }
