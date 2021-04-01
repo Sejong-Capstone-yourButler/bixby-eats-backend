1028266c0fd1de6e61db9f27e2bafa539165fdc7
zpskek <wjdghdwns0@gmail.com>
2021-04-01 23:10:16 +0900

    Create payment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,6 +23,8 @@ import { OrdersModule } from './orders/orders.module';
 import { Order } from './orders/entities/order.entity';
 import { OrderItem } from './orders/entities/order-item.entity';
 import { CommonModule } from './common/common.module';
+import { PaymentsModule } from './payments/payments.module';
+import { Payment } from './payments/entities/payment.entity';
 
 @Module({
   imports: [
@@ -69,6 +71,7 @@ import { CommonModule } from './common/common.module';
         Dish,
         Order,
         OrderItem,
+        Payment,
       ],
     }),
     // GraphQLModule처럼 설정이 있으면 Dynamic Module이다.
@@ -103,6 +106,7 @@ import { CommonModule } from './common/common.module';
     RestaurantsModule,
     OrdersModule,
     CommonModule,
+    PaymentsModule,
   ],
   controllers: [],
   providers: [],
