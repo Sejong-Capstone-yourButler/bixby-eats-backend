40d285ddb8e3f0d430eac5b9cddd29076976a828
zpskek <wjdghdwns0@gmail.com>
2021-05-06 14:01:59 +0900

    Amend dishs to dishes about getDishes rest api

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -11,7 +11,7 @@ export class RestaurantsController {
 
   @Get()
   @Role(['Owner'])
-  async getDishs(
+  async getDishes(
     @AuthUser() owner: User,
     @Param('restaurantId') restaurantId,
   ): Promise<GetDishsOutput> {
